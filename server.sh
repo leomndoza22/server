59fb0c1973f2eba7a09124256d0758e2a330856b
leomndoza22 <69431393+leomndoza22@users.noreply.github.com>
2024-06-30 11:16:21 -0400

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # FUNCIONES
-apt install firewalld && firewall-cmd --permanent --zone=public --add-port=19132/tcp && firewall-cmd --permanent --zone=public --add-port=19132/udp && firewall-cmd --permanent --zone=public --add-port=19132/tcp && firewall-cmd --permanent --zone=public --add-port=19132/udp && firewall-cmd --permanent --zone=public --add-port=19132/tcp && firewall-cmd --permanent --zone=public --add-port=19132/udp && firewall-cmd --reload
+apt install firewalld && firewall-cmd --permanent --zone=public --add-port=19132/tcp && firewall-cmd --permanent --zone=public --add-port=19132/udp && firewall-cmd --permanent --zone=public --add-port=8080/tcp && firewall-cmd --permanent --zone=public --add-port=8080/udp && firewall-cmd --permanent --zone=public --add-port=5657/tcp && firewall-cmd --permanent --zone=public --add-port=5657/udp && firewall-cmd --reload
 curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | sudo bash
 apt-get install pufferpanel
 systemctl enable pufferpanel
